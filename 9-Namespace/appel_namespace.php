42bee24d29004ebb3b2c7fbeefb8b1cdb4d8644d
Melon Deau <melondeau@MacBook-Air-de-Melon.local>
2023-03-02 14:24:20 +0100

    9 2

---
+++ 9-Namespace/appel_namespace.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'shop.php';
+USE Commerce1;// on utilise le meme namespace Commerce1
+
+$commande = new Commerce1\Commande; 
+?>
---
+++ 9-Namespace/shop.php
@@ -0,0 +1,16 @@
+<?php
+namespace Commerce1;
+class Commande{
+    public $id_commande = 1;
+}
+
+namespace Commerce2;
+class Produit {
+    public $id_produit = 14;
+}
+
+namespace Commerce3;
+class Panier{
+    public $nbProduit = 5;
+}
+?>
